ca7171fd7aa80ec769bd5cd402ca7af85c69ec97
suyog prasai <suyog224356@gmail.com>
2025-07-04 23:52:13 +0000

    added sidebar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,19 +1,37 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/globals.css";
+import { Roboto, Lato, Oswald, Bebas_Neue } from "next/font/google";
+import AuthProvider from "@/context/AuthProvider";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/layout/Sidebar";
+import { Toaster } from "@/components/ui/sonner";
 
 import { main_metadata } from "@/config/site-config";
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
   subsets: ["latin"],
+  weight: "300",
+  variable: "--font-roboto",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const oswald = Oswald({
   subsets: ["latin"],
+  weight: "700",
+  variable: "--font-oswald",
 });
+
+const bebas_neue = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-bebas_neue",
+});
+
+const lato = Lato({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-lato',
+})
+
 
 export const metadata = main_metadata;
 
@@ -25,9 +43,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.variable} ${lato.variable} ${oswald.variable} ${bebas_neue.variable} antialiased`}
       >
-        {children}
+        <AuthProvider>
+          <SidebarProvider defaultOpen={false}>
+            <AppSidebar />
+            <SidebarInset>
+              {/* Main content area */}
+              <main className="">
+                {children}
+              </main>
+              <Toaster />
+            </SidebarInset>
+          </SidebarProvider>
+        </AuthProvider>
       </body>
     </html>
   );
