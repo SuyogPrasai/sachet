da60df46112d6d95a0ccc5b890681d74ba60d374
suyog prasai <suyog224356@gmail.com>
2025-07-05 00:04:51 +0000

    added footer

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,8 +1,11 @@
+import Footer from "@/components/layout/Footer"
 import { Header } from "@/components/layout/Header"
 function Home() {
   return (
     <div>
       <Header />
+      <div className="min-h-screen"></div>
+      <Footer />
     </div>
   )
 }
